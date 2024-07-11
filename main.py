dc956ea36d7857bc62ae6e08586dbdf4d9613a14
Jesus Gollonet <jesusgollonet@gmail.com>
2024-07-11 16:18:20 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+#
+"""
+Reimplementation of https://github.com/citrusvanilla/multiplewavetracking_py as a learning experiment
+
+Near-shore ocean wave recognition through Computer Vision "recognition" workflow 
+for video sequences
+
+Pipeline consists of 4 steps
+
+1. Preprocessing
+2. Object detection
+3. Object tracking
+4. Object recognition/classification
+
+"""
